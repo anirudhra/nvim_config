c82f8fa09125d4e8915e774d1ce0cb1f3aee7c45
anirudhra <anirudhra@hotmail.com>
2024-04-26 18:26:24 -0700

    first commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+  "catppuccin",
+  lazy = false,
+  opts = {
+    transparent_background = true,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
